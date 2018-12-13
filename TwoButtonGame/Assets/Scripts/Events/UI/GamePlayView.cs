27e1d13a705a9306b4a4eaee51907432b8797fca
Lombardoh <Lombardoh@users.noreply.github.com>
2018-12-12 21:45:19 -0300

    Dispatch Play Message

---
+++ TwoButtonGame/Assets/Scripts/Events/UI/GamePlayView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayView : MonoBehaviour {
+
+    private void Awake()
+    {
+        CodeControl.Message.AddListener<GameCountdownRequestEvent>(OnGameCountDownRequested);
+    }
+
+    private void OnGameCountDownRequested(GameCountdownRequestEvent obj)
+    {
+        Debug.Log("count down");
+    }
+}
